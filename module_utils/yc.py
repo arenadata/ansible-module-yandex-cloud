d2747f8c04a7b31150e6b4f7ad1847bc0fde496d
Yulia Serova <syua@arenadata.io>
2022-01-27 13:14:59 +0300

    Try commit

--- module_utils/yc.py
+++ module_utils/yc.py
@@ -10,7 +10,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import re
 from time import sleep
 
 from ansible.module_utils.basic import AnsibleModule
