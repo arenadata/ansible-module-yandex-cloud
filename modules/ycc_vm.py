5b4019f00805143628fb56cbad5a658b17576a0e
NorthernBlow <rheinhart@yandex.ru>
2025-05-06 15:52:42 +0300

    CST-2486: fix error message when creating VM with a name like existing vm's secondary disk

--- modules/ycc_vm.py
+++ modules/ycc_vm.py
@@ -572,9 +572,9 @@ class YccVM(YC):
         err.extend(self._compare_disk(instance["bootDisk"]["diskId"], boot_disk_spec))
 
         if spec.get("secondary_disks_spec") and not instance.get("secondaryDisks"):
-            err.extend("secondary_disk not presented on instance")
+            err.append("secondary_disk not presented on instance")
         elif not spec.get("secondary_disks_spec") and instance.get("secondaryDisks"):
-            err.extend(
+            err.append(
                 "secondary_disk presented on instance but not described in module call"
             )
         elif spec.get("secondary_disks_spec") and instance.get("secondaryDisks"):
